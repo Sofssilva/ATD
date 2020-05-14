936870ec77b1199288f4fe37112d3732b94b046d
sofmeireles <sofiamfc24964@gmail.com>
2020-05-14 20:28:32 +0100

    yo

---
+++ Projeto/Projeto/main.m
@@ -0,0 +1,19 @@
+Expr={'01','02','03','04','05','06','07','08','09','10'};
+User={'01','02','03','04','05'};
+
+j = 0;	
+for i=1: size(Expr,2)
+    if mod(i,2)~= 0
+        j = j + 1;
+    end
+    filename = strcat('./RawData/acc_exp',Expr{1,i},'_user',User{1,j},'.txt');
+    delimiterIn = ' ';
+    headerlinesIn = 1;
+    format long
+    A = importdata(filename,delimiterIn,headerlinesIn);
+    dividir_graf(A.data,filename,i);
+end
+
+%Exercício 5
+accZ = A.data(:,3);
+mystft(accZ);
