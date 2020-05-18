ef33f0cbd31077c8830810bbeea094aa555b539c
FilipaCapela <50205832+FilipaCapela@users.noreply.github.com>
2020-05-18 17:03:53 +0100

    Add files via upload

--- dFt.m
+++ dFt.m
@@ -1,18 +1,22 @@
+%compor dft por causa das janelasS
+
 function dFt(data)
 labels = handler_label(1);
 aac_X = data(:,1);
 aac_Y = data(:,2);
 aac_Z = data(:,3);
 for i = 1: 12
-    xx = janelas(labels,i); 
+    matriz = janelas(labels,i);
+    tam = size(janelas(labels,i),1);
+    xx = matriz(tam,:)
     N = length(aac_X(xx(1,4):xx(1,5))); 
     Ts = 1/50;
     fs = 1/Ts;
     if (mod(N,2)==0)
-        % se o número de pontos do sinal for par
+        % se o nÃºmero de pontos do sinal for par
         f = -fs/2:fs/N:fs/2-fs/N;
     else
-        % se o número de pontos do sinal for ímpar
+        % se o nÃºmero de pontos do sinal for Ã­mpar
         f = -fs/2+fs/(2*N):fs/N:fs/2-fs/(2*N);
     end
     X = aac_X(xx(1,4):xx(1,5));
@@ -32,15 +36,17 @@ for i = 1: 12
     xlabel('[Hz]')
 end
 for j = 1: 12
-    yy = janelas(labels,j); 
+    matriz = janelas(labels,i);
+    tam = size(janelas(labels,i),1);
+    yy = matriz(tam,:)
     N = length(aac_Y(yy(1,4):yy(1,5))); %vai buscar o intervalo do primeiro q encontra da atividade em causa e calcula o N
     Ts = 1/50;
     fs = 1/Ts;
     if (mod(N,2)==0)
-        % se o número de pontos do sinal for par
+        % se o nÃºmero de pontos do sinal for par
         f = -fs/2:fs/N:fs/2-fs/N;
     else
-        % se o número de pontos do sinal for ímpar
+        % se o nÃºmero de pontos do sinal for Ã­mpar
         f = -fs/2+fs/(2*N):fs/N:fs/2-fs/(2*N);
     end
     Y = aac_Y(yy(1,4):yy(1,5));
@@ -59,19 +65,19 @@ for j = 1: 12
     title(strcat(nome,num));
     ylabel('|X|')
     xlabel('[Hz]')
-    
-    
 end
 for k = 1: 12
-    zz = janelas(labels,k); 
+    matriz = janelas(labels,i);
+    tam = size(janelas(labels,i),1);
+    zz = matriz(tam,:)
     N = length(aac_Z(zz(1,4):zz(1,5))); %vai buscar o intervalo do primeiro q encontra da atividade em causa e calcula o N
     Ts = 1/50;
     fs = 1/Ts;
     if (mod(N,2)==0)
-        % se o número de pontos do sinal for par
+        % se o nÃºmero de pontos do sinal for par
         f = -fs/2:fs/N:fs/2-fs/N;
     else
-        % se o número de pontos do sinal for ímpar
+        % se o nÃºmero de pontos do sinal for Ã­mpar
         f = -fs/2+fs/(2*N):fs/N:fs/2-fs/(2*N);
     end
     Z = aac_Z(zz(1,4):zz(1,5));
--- janelas.m
+++ janelas.m
@@ -4,7 +4,7 @@ function matriz_atividades=janelas(matriz_exp,atividade)
     j=1;
     for i=1:size(matriz_exp,1)
         if matriz_exp(i,3)==atividade
-            matriz_atividades =matriz_exp(i,:);
+            matriz_atividades(j,:) =matriz_exp(i,:);
             j=j+1;
         end
     end
