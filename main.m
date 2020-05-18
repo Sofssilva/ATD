5bc0769766c63331416207f061a9f054a7010fa8
FilipaCapela <50205832+FilipaCapela@users.noreply.github.com>
2020-05-19 00:15:12 +0100

    Add files via upload

--- main.m
+++ main.m
@@ -12,10 +12,142 @@ data10 = importfile('acc_exp10_user05.txt');
 
 
 %Exercicio 4
+%dFt(data1);
 
-dFt(data1)
+%Exercicio4.2
+exer4_2(data1,1,"x","W");
+exer4_2(data1,1,"y","W");
+exer4_2(data1,1,"z","W");
+ 
+exer4_2(data1,1,"x","W-U");
+exer4_2(data1,1,"y","W-U");
+exer4_2(data1,1,"z","W-U");
+  
+exer4_2(data1,1,"x","W-D");
+exer4_2(data1,1,"y","W-D");
+exer4_2(data1,1,"z","W-D");
+ 
+ 
+ 
+exer4_2(data2,2,"x","W");
+exer4_2(data2,2,"y","W");
+exer4_2(data2,2,"z","W");
+ 
+exer4_2(data2,2,"x","W-U");
+exer4_2(data2,2,"y","W-U");
+exer4_2(data2,2,"z","W-U");
+ 
+exer4_2(data2,2,"x","W-D");
+exer4_2(data2,2,"y","W-D");
+exer4_2(data2,2,"z","W-D");
+ 
+ 
+exer4_2(data3,3,"x","W");
+exer4_2(data3,3,"y","W");
+exer4_2(data3,3,"z","W");
+ 
+exer4_2(data3,3,"x","W-U");
+exer4_2(data3,3,"y","W-U");
+exer4_2(data3,3,"z","W-U");
+ 
+exer4_2(data3,3,"x","W-D");
+exer4_2(data3,3,"y","W-D");
+exer4_2(data3,3,"z","W-D");
+ 
+ 
+exer4_2(data4,4,"x","W");
+exer4_2(data4,4,"y","W");
+exer4_2(data4,4,"z","W");
+ 
+exer4_2(data4,4,"x","W-U");
+exer4_2(data4,4,"y","W-U");
+exer4_2(data4,4,"z","W-U");
+ 
+exer4_2(data4,4,"x","W-D");
+exer4_2(data4,4,"y","W-D");
+exer4_2(data4,4,"z","W-D");
+ 
+ 
+exer4_2(data5,5,"x","W");
+exer4_2(data5,5,"y","W");
+exer4_2(data5,5,"z","W");
+ 
+exer4_2(data5,5,"x","W-U");
+exer4_2(data5,5,"y","W-U");
+exer4_2(data5,5,"z","W-U");
+ 
+exer4_2(data5,5,"x","W-D");
+exer4_2(data5,5,"y","W-D");
+exer4_2(data5,5,"z","W-D");
+ 
+ 
+ 
+exer4_2(data6,6,"x","W");
+exer4_2(data6,6,"y","W");
+exer4_2(data6,6,"z","W");
+ 
+exer4_2(data6,6,"x","W-U");
+exer4_2(data6,6,"y","W-U");
+exer4_2(data6,6,"z","W-U");
+ 
+exer4_2(data6,6,"x","W-D");
+exer4_2(data6,6,"y","W-D");
+exer4_2(data6,6,"z","W-D"),
+ 
+ 
+ 
+ exer4_2(data7,7,"x","W");
+ exer4_2(data7,7,"y","W");
+ exer4_2(data7,7,"z","W");
+ 
+ exer4_2(data7,7,"x","W-U");
+ exer4_2(data7,7,"y","W-U");
+ exer4_2(data7,7,"z","W-U");
+ 
+ exer4_2(data7,7,"x","W-D");
+ exer4_2(data7,7,"y","W-D");
+ exer4_2(data7,7,"z","W-D");
+ 
+ 
+ 
+ exer4_2(data8,8,"x","W");
+ exer4_2(data8,8,"y","W");
+ exer4_2(data8,8,"z","W");
+ 
+ exer4_2(data8,8,"x","W-U");
+ exer4_2(data8,8,"y","W-U");
+ exer4_2(data8,8,"z","W-U");
+ 
+ exer4_2(data8,8,"x","W-D");
+ exer4_2(data8,8,"y","W-D");
+ exer4_2(data8,8,"z","W-D");
+ 
+ 
+ exer4_2(data9,9,"x","W");
+ exer4_2(data9,9,"y","W");
+ exer4_2(data9,9,"z","W");
+ 
+ exer4_2(data9,9,"x","W-U");
+ exer4_2(data9,9,"y","W-U");
+ exer4_2(data9,9,"z","W-U");
+ 
+ exer4_2(data9,9,"x","W-D");
+exer4_2(data9,9,"y","W-D");
+ exer4_2(data9,9,"z","W-D");
+ 
+
+ exer4_2(data10,10,"x","W");
+ exer4_2(data10,10,"y","W");
+ exer4_2(data10,10,"z","W");
+ 
+ exer4_2(data10,10,"x","W-U");
+ exer4_2(data10,10,"y","W-U");
+ exer4_2(data10,10,"z","W-U");
+ 
+ exer4_2(data10,10,"x","W-D");
+ exer4_2(data10,10,"y","W-D");
+ exer4_2(data10,10,"z","W-D");
+
+ 
 ponto_4_3(data1);
 
-%Exercício 5
-accZ = data10(:,3);
-mystft(accZ);
